171930e3f5fc5120e12f3585657b05aaaf17aef2
nandishswarup <nandish.swarup@fitpass.co.in>
2020-03-02 18:42:26 +0530

    small fixes

--- scripts/post.js
+++ scripts/post.js
@@ -1,28 +1,27 @@
 iseditable = false;
-count=0;
+count = 0;
 
 function onpageload() {
+    console.log('onpageload');
     var title = document.getElementById('title');
     var detail = document.getElementById('detail');
-
-    title.style.border='white'
-    detail.style.border='white'
+     title.className = title.className + " inputboxnonedit";
 
     title.value = localStorage.getItem("title");
     title.readOnly = true;
-    //title.className = document.getElementById("title").className + " input-box-nonedit";  // this adds the error class
+
     var button = document.getElementById('btnedit')
-    button.innerHTML = 'Edit    <i class="fa fa-pencil-square-o" aria-hidden="true"></i>'
+    button.innerHTML = 'Edit <i class="fa fa-pencil-square-o" aria-hidden="true"></i>'
 
 
     document.getElementById('author').innerText = localStorage.getItem("author");
 
 
-    //document.getElementById('detail').innerText=localStorage.getItem("detail");
+     detail.className = detail.className + " inputboxnonedit";
+
     detail.value = localStorage.getItem("detail");
-    // console.log(localStorage.getItem("detail")+"    new ")
-    detail.readOnly = "false";
-  //  detail.className = document.getElementById("detail").className + " input-box-nonedit";  // this adds the error class
+     detail.readOnly = "false";
+
 }
 
 function editaction() {
@@ -32,24 +31,26 @@ function editaction() {
 
     if (!iseditable) {
 
-        title.style.border='red'
-        detail.style.border='red'
+
         title.readOnly = false;
-        //title.className = title.className + " input-box-edit";  // this adds the error class
+        title.className = title.className + " input-box-edit";
         detail.readOnly = "";
-       // detail.className = detail.className + " input-box-edit";  // this adds the error class
+        detail.className = detail.className + " input-box-edit";
         var button = document.getElementById('btnedit')
         button.innerText = 'save'
         iseditable = true
     } else {
-        title.style.border='white'
-        detail.style.border='white'
-        title.style.borderWidth ="2px";
-        detail.style.borderWidth ="2px";
-        localStorage.setItem("title",title.value);
-        localStorage.setItem("detail",detail.value);
-        onpageload()
 
+        localStorage.setItem("title", title.value);
+        localStorage.setItem("detail", detail.value);
+
+        detail.readOnly = "false";
+        title.readOnly = true;
+
+         document.getElementById('title').style.borderColor = 'transparent'
+        document.getElementById('detail').style.borderColor = 'transparent'
+        var button = document.getElementById('btnedit')
+        button.innerHTML = 'Edit <i class="fa fa-pencil-square-o" aria-hidden="true"></i>'
         iseditable = false
 
     }
@@ -63,34 +64,26 @@ function auto_grow(element) {
 }
 
 
-function addcomment()
-{
-    var button=document.getElementById('addcomment')
-    var textarea=document.getElementById('inputComment')
-    var commentsection=document.getElementById('commentsection')
-    if(textarea.value.length>0)
-    {
+function addcomment() {
+    var button = document.getElementById('addcomment')
+    var textarea = document.getElementById('inputComment')
+    var commentsection = document.getElementById('commentsection')
+    if (textarea.value.length > 0) {
 
-        commentsection.innerHTML+='<p class="comment-item">'+textarea.value+'</p>'
+        commentsection.innerHTML += '<p class="comment-item">' + textarea.value + '</p>'
 
     }
-    textarea.value=''
-
-
+    textarea.value = ''
 
 
 }
-function likeaction()
-{
-    var like=document.getElementById('likenumber')
+
+function likeaction() {
+    var like = document.getElementById('likenumber')
 
 
     count++;
-    like.innerText=count+' person likes this!'
-
-
-
-
+    like.innerText = count + ' person likes this!'
 
 
 }
